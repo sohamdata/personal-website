4600732e0e71bd518aa56d2d7eea35a3a031d082
Soham Datta <sohamdatta34@gmail.com>
2023-02-28 23:07:50 +0530

    added fun

--- src/components/Fun.js
+++ src/components/Fun.js
@@ -1,6 +1,22 @@
+import React, { useEffect } from 'react';
 import { Hash, Sort } from './_fun';
 import Background from './Background';
+import closer2 from '../assets/closer2.aac';
+
 export default function Fun() {
+  const audio = new Audio(closer2);
+  audio.volume = 0.2;
+
+  useEffect(() => {
+    // Play audio when Fun page is mounted
+    audio.play();
+
+    // Pause audio when Fun page is unmounted
+    return () => {
+      audio.pause();
+    }
+  }, [audio]);
+
   return (
     <div>
       <Background />
