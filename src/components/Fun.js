91684f10ca3db526487bdf6791595889715509df
Soham Datta <sohamdatta34@gmail.com>
2023-02-28 23:10:34 +0530

    fixed audio; added useMemo

--- src/components/Fun.js
+++ src/components/Fun.js
@@ -1,20 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Hash, Sort } from './_fun';
 import Background from './Background';
 import closer2 from '../assets/closer2.aac';
 
 export default function Fun() {
-  const audio = new Audio(closer2);
-  audio.volume = 0.2;
+  const audio = useMemo(() => new Audio(closer2), []);
+  // useMemo is used to prevent the audio from being reloaded on every render
 
   useEffect(() => {
     // Play audio when Fun page is mounted
+    audio.volume = 0.2;
     audio.play();
 
     // Pause audio when Fun page is unmounted
-    return () => {
-      audio.pause();
-    }
+    return () => audio.pause();
   }, [audio]);
 
   return (
