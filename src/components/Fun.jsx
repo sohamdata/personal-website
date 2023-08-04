7fbbedbaf842a97953f5dc294b7a4b1604eab4cf
Soham Datta <sohamdatta34@gmail.com>
2023-08-04 21:28:18 +0530

    migrate: cra to vite

--- src/components/Fun.jsx
+++ src/components/Fun.jsx
@@ -2,21 +2,23 @@ import React, { useEffect, useMemo } from 'react';
 import { Hash, Sort, BinaryString } from './FunItems';
 import Background from './Background';
 
-const musicContext = require.context('../assets/music/', false, /\.(aac)$/);
-const musicFiles = musicContext.keys().map(musicContext);
+const songs = [
+  'closer2.aac',
+  'peaked.aac',
+];
 
 export default function Fun() {
-  const randomSongIndex = Math.floor(Math.random() * musicFiles.length);
-  const audio = useMemo(() => new Audio(musicFiles[randomSongIndex]), [randomSongIndex]);
+  const randomSongIndex = Math.floor(Math.random() * songs.length);
+  const audio = useMemo(() => new Audio(`../../src/assets/music/${songs[randomSongIndex]}`), [randomSongIndex]);
 
   useEffect(() => {
-    audio.loop = true;
-    audio.volume = 0.3;
     let playing = audio.play();
+
     if (playing !== undefined) {
-      playing
-        .then(_ => {
-        })
+      playing.then(_ => {
+        audio.loop = true;
+        audio.volume = 0.3;
+      })
         .catch(error => {
           console.log(error);
         });
